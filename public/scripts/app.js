d5f28e5acd3e1ca4d53641003a21353d875420fa
Longbottom <U333033@euro.iberdrola.local>
2018-11-16 16:24:07 +0000

    Hello

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,21 +3,68 @@ var watsonChatbot = angular.module('watsonChatbot', []);
 watsonChatbot.controller('mainController', ['$scope', '$http',
   function mainController($scope, $http) {
 
-    $scope.usrMsg = 'Hello';
+    $scope.usrMsg = '';
+    $scope.messages = [];
+    $scope.responseIsOption = false;
 
-    $scope.sendMsg = function () {
-      $scope.messages.push({text: $scope.usrMsg, bot: false});
-      $http({
-        method: 'POST',
-        url: 'http://localhost:3000/msg',
-        data: {message: $scope.usrMsg}
-      }).then(function successCallback(response) {
-        console.log(response);
-        $scope.messages.push(response.data);
-      }, function errorCallback(response) {
-        console.log('Error:', response);
-      });
+    var processResponse = function (httpResponse) {
+      console.log(httpResponse.data);
+      for (var i =0; i < httpResponse.data.length; i++) {
+        var response = httpResponse.data[i];
+        $scope.messages.push(response);
+      }
+      var numberOfMessages = $scope.messages.length;
+      $scope.responseIsOption = ($scope.messages[numberOfMessages-1]['response_type'] === 'option');
+      var modal = document.getElementById('chat-modal');
+      //  Delays the forced scroll allow the page to add the elements into the containers
+      setTimeout($scope.scroll, 200);
+    }
+
+    var getTimeString = function (timestamp) {
+      var timeAsString = '';
+      var minutes = timestamp.getMinutes();
+      var minAsString = minutes <= 9 ? '0' + minutes : minutes.toString();
+      var timeAsString = timestamp.getHours() + ':' + minAsString;
+      return timeAsString;
+    }
+
+    $scope.sendMsg = function (userMessage) {
+      var message;
+      if (userMessage !== undefined) {
+        message = userMessage;
+      } else {
+        var message = $scope.usrMsg;
+      }
+
+      if (message !== '') {
+        $scope.usrMsg ='';
+        var now = new Date();
+        var time = getTimeString(now);
+        $scope.messages.push({text: message, bot: false, time: time, response_type: 'none'});
+        $http({
+          method: 'POST',
+          url: 'http://localhost:3000/msg',
+          data: {message: message}
+        }).then(function successCallback(response) {
+          processResponse(response);
+        }, function errorCallback(response) {
+          console.log('Error:', response);
+        });
+      }
     };
 
-    $scope.messages = [];
+    $scope.respondToOption = function (value) {
+      $scope.sendMsg(value.input.text);
+      console.log(value);
+    };
+
+    $scope.reset = function () {
+      $scope.usrMsg = "__abc123";
+      $scope.sendMsg();
+    };
+
+    $scope.scroll = function () {
+        var modal = document.getElementById('chat-modal');
+        modal.scrollTop = modal.scrollHeight
+    }
   }]);
--- server.js
+++ server.js
@@ -1,69 +1,142 @@
+// Libraries
+var assistantV1 = require('watson-developer-cloud/assistant/v1'); // Loading the Watson SDK
 var bodyParser = require('body-parser');
-var express = require('express');
-var _ = require('lodash');
-var path = require('path');
-var app = express();
-var credentials = require(path.join(__dirname, 'credentials.json'));
+var express = require('express'); // ExpressJS HTTP framework
+var _ = require('lodash'); // Tools to manipulate objects, arrays, string...
+var path = require('path'); // Node library to manipulate directories, file paths, etc.
 
-var assistantV1 = require('watson-developer-cloud/assistant/v1');
+var app = express(); // Instance of Express
 
+var credentials = require(path.join(__dirname, 'credentials.json')); // Loading the credentials from 'credentials.json'
 var WORKSPACE_ID = _.get(credentials, 'workspace_id');
 var USERNAME = _.get(credentials, 'username');
 var PASSWORD = _.get(credentials, 'password');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, '/public')));
+// Configuration of the app to use libraries
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var port = 3000;
 
+let context = {}; // Context for Watson
+
+// Watson service is created with credentials and init paramneters
 var service = new assistantV1({
   username: USERNAME,
   password: PASSWORD,
-  version: '2018-07-10',
-  minimum_confidence: 0.50, // (Optional) Default is 0.75
+  version: '2018-09-20',
+  minimum_confidence: 0.75
 });
 
-service.message({
-  workspace_id: WORKSPACE_ID
-}, processResponse);
+var processTextResponse = function (response) {
+  return {text: response.text};  //object to be return the the client in the http response
+};
 
-//
+var processOptionResponse = function (response) {
+  return response;
+};
+
+// Function that processes the response from Watson
 var processResponse = function (err, response) {
   var intent;
-  var text;
+  var text = '';
+  var now = new Date();
+  var time = '';
+  var time = now.getHours() + ':' + now.getMinutes();
+
   if (err) {
     return console.log(err);
   }
 
+  context = response.context;
+  console.log(JSON.stringify(response, null, 2));
+
+  // If some intents are detected
   if (response.intents.length > 0) {
+//    console.log('Intents:');
     intent = response.intents[0].intent
-    console.log('Detected intents: #', intent);
-  }
+    // _.forEach(response.intents, function (intent, key) {
+    //     if (_.isEqual(intent.intent, 'change_bank_details')) {
+    //       console.log('******** DO AN API CALL TO ORACLE HR BLAH');
+    //     }
+    //     console.log(key, intent);
+    // });
 
-  if (response.output.generic.length != 0) {
-    text = response.output.generic[0].text;
-    console.log(text);
+    // used to look for a bot response, and ouput them line after line
+    var botResponses = response.output.generic; //Is an array of Objects {response_type: "blah", param1: value1, param2, value2, ...}
+    var responses = [];
+      if (botResponses.length != 0) { // If not empty, concatenate all the messages
+        // For all each of response in the array of response
+        // apply function to array botResponses
+        responses = _.map(botResponses, function (botResponse) {
+          var response;
+          var responseType = _.get(botResponse, 'response_type');
+          console.log('-------------------- Response type:', responseType);
+          if (_.isEqual(responseType, 'text')) {
+            response = processTextResponse(botResponse);
+//         text += response.text + '\n';
+          } else if (_.isEqual(responseType, 'option')) {
+            response = processOptionResponse(botResponse);
+          }
+        // TO DO: add flag for the bot, add the time, add the intents
+        _.set(response, 'bot', true);
+        _.set(response, 'time', time);
+        _.set(response, 'intent', intent);
+        return response;
+      });
+      console.log('-------------------- Responses as set to the client', responses);
+    }
   }
-  return {bot: true, text: text, intent: intent};
-};
+  return responses;
+}
 
+// APP STARTS HERE
+
+// Initial message to Watson, should display an error message if Watson down
+service.message({
+  workspace_id: WORKSPACE_ID,
+  context: context
+}, processResponse);
+
+// When the client performs a GET request on 'http://localhost:3000/', sending 'index.html'
 app.get('/', function (req, res){
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index1.html'));
 });
 
-app.post('/msg', function (req, res) {
-  var message = _.get(req, 'body.message');
-  console.log(message);
 
+// When the client performs a POST request to 'http://loclahost:3000/msg'
+app.post('/msg', function (req, res) {
+  var userMessage = _.get(req, 'body.message');
+  console.log('');
+  console.log('-------------------');
+  console.log('User message: ', userMessage);
+
+  // Relaying the message to Watson
   service.message({
     workspace_id: WORKSPACE_ID,
-    input: {text: message}
+    input: {text: userMessage},
+    context : context,
   }, function (err, response) {
       var data = processResponse(err, response);
       res.json(data);
   });
 });
+
+// app.get('/reset', function (req, res) {
+//   console.log('Resetting the context');
+//   context = {};
+//   // Relaying the message to Watson
+//   service.message({
+//     workspace_id: WORKSPACE_ID,
+//     input: {text: ''},
+//     context : context,
+//   }, function (err, response) {
+//       var data = processResponse(err, response);
+//       console.log(data)
+//       res.json(data);
+//   });
+// });
 
 app.listen(port, function (err) {
   if (err) {
